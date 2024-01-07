da9b6b8a3784dca4bc7b1ecafeaedd747c2df18a
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-07 19:33:05 -0300

    creating page home

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,10 @@
 import { Container } from "./styles";
-import { InputSearchContainer } from "./styles";
 
 
 export default function Header() {
   return (
     <Container>
       <h1>MyContacts</h1>
-
-      <InputSearchContainer>
-        <input type="text"  placeholder="Pesquise pelo nome... "></input>
-
-      </InputSearchContainer>
     </Container>
 
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,6 +8,7 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;
@@ -99,3 +100,24 @@ export const Card = styled.div`
     }
   }
 `;
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0,0,0, 0.84);
+    outline: 0;
+    padding: 0 16px;
+  }
+
+  &::placeholder {
+    color: #BCBCBC;
+  }
+
+`;
+

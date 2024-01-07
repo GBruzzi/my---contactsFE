a9d1f9f722aff2d509cbf1fac2637a9546b52844
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-07 20:26:30 -0300

    add input e select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,8 +1,21 @@
 import PageHeader from "../../components/PageHeader";
 
+import Input from "../../components/Input";
+import Select from "../../components/Select";
+
 export default function NewContact() {
   return (
-    <PageHeader title = 'Cria novo contato'/>
+    <>
+      <PageHeader title="Cria novo contato" />
 
+      <Input type="text"  placeholder="Nome"/>
+
+      <Select>
+        <option value='123'>Instagram</option>
+        <option value='123'>Twitter</option>
+        <option value='123'>Face</option>
+
+      </Select>
+    </>
   );
 }

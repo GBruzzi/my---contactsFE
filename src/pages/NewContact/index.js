fa764c8a2c81bfaefe08d798d3c2646edb1f67ed
GBruzzi <gbruzzi@sga.pucminas.br>
2024-01-07 21:13:44 -0300

    add button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -2,20 +2,24 @@ import PageHeader from "../../components/PageHeader";
 
 import Input from "../../components/Input";
 import Select from "../../components/Select";
+import Button from "../../components/Button";
 
 export default function NewContact() {
   return (
     <>
       <PageHeader title="Cria novo contato" />
 
-      <Input type="text"  placeholder="Nome"/>
+      <Input type="text" placeholder="Nome" />
 
       <Select>
-        <option value='123'>Instagram</option>
-        <option value='123'>Twitter</option>
-        <option value='123'>Face</option>
-
+        <option value="123">Instagram</option>
+        <option value="123">Twitter</option>
+        <option value="123">Face</option>
       </Select>
+
+      <Button type="button">Salvar alterações</Button>
+
+      <Button type="button"  disabled>Salvar alterações</Button>
     </>
   );
 }
